62a496702b784c1f8c52c3810712a27c55e27814
Leo Spratt <enchant97@users.noreply.github.com>
2022-06-04 13:35:54 +0100

    add https healthcheck

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -2,9 +2,9 @@
 
 if [ -n "$CERT_FILE" ] && [ -n "$KEY_FILE" ]
 then
-    # TODO implement https health check
-    echo "https health check not supported yet :("
+    args="https://127.0.0.1:$PORT/is-healthy --allow-unverified"
 else
     args="http://127.0.0.1:$PORT/is-healthy"
-    python -m web_health_checker $args
 fi
+
+python -m web_health_checker $args
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,6 @@ args="--bind $BIND --workers $WORKERS --log-level $LOG_LEVEL"
 if [ -n "$CERT_FILE" ] && [ -n "$KEY_FILE" ]
 then
     args="$args --certfile $CERT_FILE --keyfile $KEY_FILE"
-    # TODO remove warning when https health check is implemented
-    echo "https health check not supported yet :("
 fi
 
 hypercorn 'web_portal.main:create_app()' $args
