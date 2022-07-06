b160c88c6261b436e89c426314b7964b79dba53f
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-06 14:07:20 +0100

    update to v2 theme-changer

--- web_portal/static/script.js
+++ web_portal/static/script.js
@@ -1,38 +1,11 @@
-ThemeChanger.themes = {
-    os: {
-        name: "OS",
-        css: null,
-    },
-    light: {
-        name: "Light",
-        css: [
-            ["color-scheme", "light"],
-            ["--font-dark", "var(--font-dark--light)"],
-            ["--font-light", "var(--font-light--light)"],
-            ["--bg-bnt", "var(--bg-bnt--light)"],
-            ["--bg-body", "var(--bg-body--light)"],
-            ["--bg-sub-body", "var(--bg-sub-body--light)"],
-            ["--bg-panel", "var(--bg-panel--light)"],
-        ]
-    },
-    dark: {
-        name: "Dark",
-        css: [
-            ["color-scheme", "dark"],
-            ["--font-dark", "var(--font-dark--dark)"],
-            ["--font-light", "var(--font-light--dark)"],
-            ["--bg-bnt", "var(--bg-bnt--dark)"],
-            ["--bg-body", "var(--bg-body--dark)"],
-            ["--bg-sub-body", "var(--bg-sub-body--dark)"],
-            ["--bg-panel", "var(--bg-panel--dark)"],
-        ]
-    },
-};
+"use-strict";
 
 ThemeChanger.theme_picker_parent = document.querySelector("main");
 ThemeChanger.use_local = true;
 ThemeChanger.selected_theme_css_class = "green";
 
-document.getElementById("themeToggleBnt").addEventListener("click", ThemeChanger.toggle_theme_picker);
+let themeToggleBnt = document.getElementById("themeToggleBnt")
+themeToggleBnt.addEventListener("click", ThemeChanger.toggle_theme_picker);
+themeToggleBnt.classList.remove("hidden");
 
 ThemeChanger.on_load();
