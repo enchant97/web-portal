a39b95bd081380254b989e271bfa30fd76962443
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-12 12:38:39 +0100

    add librejs license magnet links to js

--- web_portal/plugins/core/static/core.js
+++ web_portal/plugins/core/static/core.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0-or-later
 "use-strict";
 
 const core__sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
@@ -37,3 +38,4 @@ function core__clock_update(time_display, date_display) {
         date_display.innerText = date_text;
     }
 }
+// @license-end
--- web_portal/static/script.js
+++ web_portal/static/script.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0-or-later
 "use-strict";
 
 ThemeChanger.theme_picker_parent = document.querySelector("main");
@@ -9,3 +10,4 @@ themeToggleBnt.addEventListener("click", ThemeChanger.toggle_theme_picker);
 themeToggleBnt.classList.remove("hidden");
 
 ThemeChanger.on_load();
+// @license-end
